b584f0b0f266f62c7e0e1828fba105dfe98c1926
José Miguel Gómez Fernández <91533325+josemiguelgomezf@users.noreply.github.com>
2023-10-04 12:54:38 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,18 @@
+# Definir la base de datos
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Definir las aplicaciones instaladas
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'philochat',
+]
